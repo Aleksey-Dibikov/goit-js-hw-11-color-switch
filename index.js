f85f828264ff19cff6010e0701e863bdd962c875
Aleksey Dibikov <dibikovaleksey@gmail.com>
2021-08-14 14:57:31 +0300

    adds js code, solution task

--- index.js
+++ index.js
@@ -10,3 +10,32 @@ const colors = [
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+
+const refs = {
+  body: document.querySelector('body'),
+  btnStart: document.querySelector('[data-action="start"]'),
+  btnStop: document.querySelector('[data-action="stop"]')
+}
+
+refs.btnStart.addEventListener('click', changeColor);
+refs.btnStop.addEventListener('click', stopChangeColor);
+
+let isActive = false;
+let colorId = null;
+
+function changeColor() {
+  if (isActive) {
+    return;
+  }  
+  colorId = setInterval(() => {
+    refs.body.style.background = colors[randomIntegerFromInterval(0, colors.length - 1)]
+  }, 1000);
+  isActive = true;
+  // console.log('start');
+};
+
+function stopChangeColor() {  
+  clearInterval(colorId)
+  isActive = false;
+  // console.log('stop');
+}
